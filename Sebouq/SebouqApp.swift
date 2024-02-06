fc8f4cbba9b567b83b28e7d959c3a0425fd106a2
v4rli <rzakri@twq.idserve.net>
2024-02-06 21:48:18 +0300

    Initial Commit

---
+++ Sebouq/SebouqApp.swift
@@ -0,0 +1,17 @@
+//
+//  SebouqApp.swift
+//  Sebouq
+//
+//  Created by roaa on 25/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SebouqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
